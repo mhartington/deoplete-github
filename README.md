0b70d4a334c015ed3d40ebf5058aea79018d1f9d
mhartington <mikehartington@gmail.com>
2017-01-10 21:28:16 -0500

    refactor(): refactor and cleanup

--- README.md
+++ README.md
@@ -1,47 +1,19 @@
 # Deoplete Github
 
-## Dependencies
-
-I've tried not to replicate existing behaviour from already great plugins:
-
-- [deoplete][]
-- [fugitive][]
-- [rhubarb][]
+This is a fork of [this plugin](https://github.com/SevereOverfl0w/deoplete-github). 
+While the original relied on fugitive.vim and rhubarb, is dependency free.
 
 ## Install
 
 Install with your favourite vim plugin manager:
 
 ```vim
-Plug 'SevereOverfl0w/deoplete-github'
+call dein#add('mhartington/deoplete-github')
 ```
 
-Add it as a deoplete source:
-
-```vim
-let g:deoplete#sources = {}
-let g:deoplete#sources.gitcommit=['github']
-```
-
-Deoplete also needs a little configuration:
-
-```vim
-let g:deoplete#keyword_patterns = {}
-let g:deoplete#keyword_patterns.gitcommit = '.+'
-
-call deoplete#util#set_pattern(
-  \ g:deoplete#omni#input_patterns,
-  \ 'gitcommit', [g:deoplete#keyword_patterns.gitcommit])
-
-```
 
 # Usage
 
 In your git commit message:
 
 - The prefix "#" will find issue numbers for auto-completion.
-- You can also auto-complete github issue titles.
-
-[fugitive]: https://github.com/tpope/vim-fugitive
-[rhubarb]: https://github.com/tpope/vim-rhubarb
-[deoplete]: https://github.com/Shougo/deoplete.nvim
--- rplugin/python3/deoplete/sources/github.py
+++ rplugin/python3/deoplete/sources/github.py
@@ -1,79 +1,109 @@
-from .base import Base
 import netrc
 import re
-from urllib.parse import urlparse
-import urllib.request as request
 import json
 import base64
+from subprocess import Popen, PIPE
+import urllib.request as request
+from urllib.parse import urlparse
+from deoplete.source.base import Base
+from deoplete.util import error
 
-def repo_homepage(vim):
-    """Return the repo homepage, akin to rhubarb#repo_request
-    function
-
-    :returns: String like "https://github.com/user/repo"
-    """
-
-    repo_url = vim.eval('fugitive#repo().config("remote.origin.url")')
-    homepage = vim.call('rhubarb#homepage_for_url', repo_url)
-    return homepage
-
-def repo_base(vim):
-    """
-    :vim: Vim Object
-    :returns: API endpoint for current repo
-    """
-    base = repo_homepage(vim)
-    if base:
-        if re.search('//github\.com/', base) is not None:
-            base = base.replace('//github.com/', '//api.github.com/repos/')
-        else:
-            # I'm not sure how to work this
-            # It's enterprise github, I don't understand vim regex
-            base = "failure"
-            pass
-
-    return base
-
-def authenticator(hostname):
-    """Parse netrc file into a dict
-
-    :hostname: Hostname to get authenticator for
-    :returns: Dict with login, account and password key
-
-    """
-    myrc = netrc.netrc()
-    authenticator = myrc.authenticators(hostname)
-
-    return {'login': authenticator[0],
-            'account': authenticator[1],
-            'password': authenticator[2]}
 
 class Source(Base):
 
-    """Fetches issues from Github API."""
-
     def __init__(self, vim):
+        """
+        Base init
+        """
         Base.__init__(self, vim)
 
         self.name = 'github'
         self.mark = '[GH]'
-        self.filetypes = ['gitcommit']
-        self.input_pattern = '#\w+'
+        self.filetypes = ['gitcommit', 'magit']
+        self.debug_enabled = True
+        self.input_pattern = '#'
+
+    def log(self, message):
+        """
+        Log message to vim echo
+        """
+        self.debug('*' * 10)
+        self.debug(message)
+        self.debug('*' * 10)
+
+    def repo_homepage(self):
+        """Return the repo homepage, akin to rhubarb#repo_request
+        function
+        :returns: String like "https://github.com/user/repo"
+        """
+        proc = Popen(["git", "config", "--get",
+                      "remote.origin.url"], stdout=PIPE)
+        out, err = proc.communicate()
+
+        if err is not None:
+            error(self.vim, 'Theres been an error')
+        repoConfig = str(out.decode('utf-8'))
+
+        if 'https' in repoConfig:
+            self.log('repoConfig: ' + repoConfig)
+            url_fragments = repoConfig.strip(".git\n")
+            homepage = url_fragments
+        else:
+            url_fragments = repoConfig.strip("git\n").strip('.').split(':')
+            homepage = 'https://github.com/' + url_fragments[1]
+        return homepage
+
+    def repo_base(self):
+        """
+        :returns: API endpoint for current repo
+        """
+        base = self.repo_homepage()
+        if base:
+            if re.search('//github\.com/', base) is not None:
+                base = base.replace('//github.com/', '//api.github.com/repos/')
+            else:
+                # I'm not sure how to work this
+                # It's enterprise github, I don't understand vim regex
+                base = "failure"
+                pass
+
+        return base
+
+    def authenticator(self, hostname):
+        """Parse netrc file into a dict
+
+        :hostname: Hostname to get authenticator for
+        :returns: Dict with login, account and password key
+
+        """
+        myrc = netrc.netrc()
+        authenticator = myrc.authenticators(hostname)
+
+        return {'login': authenticator[0],
+                'account': authenticator[1],
+                'password': authenticator[2]}
+
+    def get_complete_position(self, context):
+        """
+        returns the cursor position
+        """
+        m = re.search(r"\w*$", context["input"])
+        return m.start() if m else -1
 
     def gather_candidates(self, context):
         """Gather candidates from github API
         """
 
-        base = repo_base(self.vim)
-
+        base = self.repo_base()
         if base:
             base = base + '/issues?per_page=200'
 
             base_url = urlparse(base)
-            credentials = authenticator(base_url.hostname)
+            credentials = self.authenticator(base_url.hostname)
 
             r = request.Request(base)
-            creds = base64.encodestring(bytes('%s:%s' % (credentials.get('login'), credentials.get('password')), 'utf-8')).strip()
+            creds = base64.encodestring(bytes('%s:%s' % (credentials.get(
+                'login'), credentials.get('password')), 'utf-8')).strip()
             r.add_header('Authorization', 'Basic %s' % creds.decode('utf-8'))
 
             with request.urlopen(r) as req:
@@ -81,13 +111,9 @@ class Source(Base):
                 response = json.loads(response_json)
 
                 titles = [x.get('title', '') for x in response]
-                numbers = [{'word': '#' + str(x.get('number', '')),
-                            'menu': x.get('title'),
-                            'info': x.get('body')}
-                        for x in response]
-                titles = [{'word': x.get('title'),
-                           'menu': '#' + str(x.get('number', '')),
-                           'info': x.get('body')}
+                numbers = [{'word': str(x.get('number', '')),
+                            'menu': x.get('title')
+                            }
                            for x in response]
-                return numbers + titles
+                return numbers
         return []
